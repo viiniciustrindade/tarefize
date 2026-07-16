805cfc505b3190c2142a34040a56898f864fe4af
Vinicius Trindade <vinicius.trindade@edu.sc.senai.br>
2026-07-16 14:58:57 -0300

    feat: add validation annotations for email in FindUserByEmailRequestDto

--- todolist/src/main/java/com/senai/todolist/api/dto/user/FindUserByEmailRequestDto.java
+++ todolist/src/main/java/com/senai/todolist/api/dto/user/FindUserByEmailRequestDto.java
@@ -1,6 +1,11 @@
 package com.senai.todolist.api.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record FindUserByEmailRequestDto(
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "O e-mail deve ser válido")
     String email
 ) {
 } 
